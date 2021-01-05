316657b0ceb38011bfb2c788d58d346258df1351
Jacob Joseph <jakejose@iu.edu>
2021-01-04 22:24:56 -0800

    html updates

--- index.js
+++ index.js
@@ -4,5 +4,4 @@ console.log ('working');
 $(document).ready(function(){
     $('.header').height($(window).height());
   })
-
 //<a href='https://postimg.cc/v4d5ZWpt' target='_blank'><img src='https://i.postimg.cc/v4d5ZWpt/david-klein-s-NAi-PQ7hdw-unsplash.jpg' border='0' alt='david-klein-s-NAi-PQ7hdw-unsplash'/></a>
